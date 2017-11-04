d0168c71700b87d6a38a80c22be5fb79e1e58ecb
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-11-04 21:26:30 +0100

    Añado la clase Contact

---
+++ src/uva/tds/pr1/equipo05/Contact.java
@@ -0,0 +1,45 @@
+package uva.tds.pr1.equipo05;
+/**
+ * 
+ * @author ismpere
+ * @author martorb
+ *
+ */
+public class Contact {
+	String id;
+	/**
+	 * Constructor por defecto de la clase Contact
+	 * @assert.pre !id.equals(null)
+	 * @assert.pre !id.equals("")
+	 * @assert.pre id.length()<=20
+	 * @param id
+	 * 
+	 */
+	public Contact(String id){
+		assert(!id.equals(null));
+		assert(!id.equals(""));
+		assert(id.length()<=20);
+		this.id = id;
+	}
+	/**
+	 * Devuelve el ID del contacto
+	 * @return ID
+	 */
+	public String getID(){
+		return id;
+	}
+	/**
+	 * Cambia el ID del contacto
+	 * @assert.pre !id.equals(null)
+	 * @assert.pre !id.equals("")
+	 * @assert.pre id.length()<=20
+	 * @param id
+	 */
+	public void setID(String id){
+		assert(!id.equals(null));
+		assert(!id.equals(""));
+		assert(id.length()<=20);
+		this.id = id;
+	}
+	
+}
