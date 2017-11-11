d16acd4cb35001bc93730c8799a90849aa0c8edc
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-11-11 17:52:49 +0100

    Añado mas casos de prueba

--- src/uva/tds/pr1/equipo05/Main.java
+++ src/uva/tds/pr1/equipo05/Main.java
@@ -7,8 +7,16 @@ public class Main{
 	public static void main(String[] args){
 		EContactSystemInterface contacts = EContactSystemImpl.contactSystemFactory();
 		Path ruta = Paths.get("src/uva/tds/pr1/equipo05/libreta_electronica.xml");
+		Path ruta2 = Paths.get("libreta_electronica.xml");
+		Path ruta3 = Paths.get("libreta_electronica2.xml");
 		contacts.loadFrom(ruta);
 		String[] correos = {"andrea@gmail.com"};
-		contacts.createNewPerson("Andrea", "andalon", null, correos, null);
+		contacts.createNewPerson("Andrea", "andalon", "Alonso", correos, null);
+		contacts.updateTo(ruta2);
+		
+		String[] correos2 = {"yania@gmail.com"};
+		contacts.loadFrom(ruta2);
+		contacts.createNewPerson("Yania", "ycg", "Crespo", correos2, null);
+		contacts.updateTo(ruta3);
 	}
 }
