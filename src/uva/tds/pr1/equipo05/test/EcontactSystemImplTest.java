cd97a2d63f40b5fef968192432e8fef335f9161b
Mario <mariotorbado.96@gmail.com>
2017-11-11 23:31:57 +0100

    Añado la clase del test
    (por completar)

---
+++ src/uva/tds/pr1/equipo05/test/EcontactSystemImplTest.java
@@ -0,0 +1,17 @@
+package uva.tds.pr1.equipo05.test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.xmlunit.builder.*;
+import org.xmlunit.diff.*;
+
+import uva.tds.pr1.equipo05.EContactSystemImpl;;
+
+public class EcontactSystemImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
