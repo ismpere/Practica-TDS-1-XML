782ce8651e092fe643d729e2be45aca937611e13
Mario <mariotorbado.96@gmail.com>
2017-11-12 12:26:00 +0100

    Añado primer test

--- src/uva/tds/pr1/equipo05/test/EcontactSystemImplTest.java
+++ src/uva/tds/pr1/equipo05/test/EcontactSystemImplTest.java
@@ -1,17 +1,43 @@
 package uva.tds.pr1.equipo05.test;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+import java.nio.file.*;
+import java.io.*;
+
+import org.junit.*;
 import org.xmlunit.builder.*;
 import org.xmlunit.diff.*;
 
-import uva.tds.pr1.equipo05.EContactSystemImpl;;
+import uva.tds.pr1.equipo05.*;
+
 
 public class EcontactSystemImplTest {
-
+	
+	@AfterClass
+	public static void tearDownGeneral() throws IOException{
+		Path dir = Paths.get(System.getProperty("user.id"),"test/xmlTestFiles/");
+		DirectoryStream<Path> archivos_a_borrar = Files.newDirectoryStream(dir,"*.out.xml");
+		for(Path path : archivos_a_borrar){
+			Files.delete(path);
+		}
+		archivos_a_borrar.close();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLoadFrom() {
+		//carga un XML de prueba desde una carpeta
+		EContactSystemInterface libreta = EContactSystemImpl.contactSystemFactory();		
+		Path entrada_xml = Paths.get(System.getProperty("user.id"),"test/xmlTestFiles/testLoadFrom.xml");
+		libreta.loadFrom(entrada_xml);
+		Path salida_xml = Paths.get(System.getProperty("user.id"),"test/xmlTestFiles/testLoadFrom.out.xml");
+		libreta.updateTo(salida_xml);
+		Diff miDiff = DiffBuilder.compare(entrada_xml.toFile()).withTest(salida_xml.toFile()).checkForIdentical()
+					  .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.Default)).build();
+		
+		assertTrue(libreta.isXMLLoaded());
+		assertFalse(libreta.isModifiedAfterLoaded());
+		assertFalse("¿Son los XML idénticos? "+miDiff.toString(),miDiff.hasDifferences());
+		
 	}
 
 }
